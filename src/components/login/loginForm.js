3882f7a4825ef5752665990256c8268bb9f24b1a
BIT Student <classroom@bgit.rs>
2017-11-21 17:20:09 +0100

    add bootstrap n styling

--- src/components/login/loginForm.js
+++ src/components/login/loginForm.js
@@ -11,14 +11,14 @@ class LoginForm extends React.Component {
     }
 
 
-    initState(){
-        return{
+    initState() {
+        return {
             email: "",
             password: ""
         };
 
     }
-    
+
     bindEventHandlers() {
         this.handleInputChange = this.handleInputChange.bind(this);
     }
@@ -34,24 +34,38 @@ class LoginForm extends React.Component {
 
     logIn() {
         event.preventDefault();
-        
+
     }
 
 
 
     render() {
         return (
-            <main>
+            <main className="form">
                 <div>
-                    <Link to="/login"><button>Login</button></Link>
-                    <Link to="/register"><button>Register</button></Link>
+                    <Link to="/login"><button className="btn btn-light">Login</button></Link>
+                    <Link to="/register"><button  className="btn btn-light">Register</button></Link>
                 </div>
+
                 <form>
-                    <input type="email" name="email" placeholder="Email Address" value={this.state.email} onChange={this.handleInputChange} />
-                    <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleInputChange} />
-                    <input type="submit" value="Login" onClick={this.logIn} />
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email" value={this.state.email} onChange={this.handleInputChange} />
+                        <small id="emailHelp" className="form-text text-muted">We will never share your email with anyone else.</small>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" name="password" value={this.state.password} onChange={this.handleInputChange} />
+                    </div>
+                
+                    <button type="submit" className="btn btn-primary" onClick={this.logIn}>Login</button>
+
                 </form>
+
+
             </main>
+
+
         );
     }
 
