4c1131e3ab0383864a2b226955615aa87c4573f8
BIT Student <classroom@bgit.rs>
2017-11-22 16:54:23 +0100

    code style header

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 class Header extends React.Component {
     constructor(props) {
         super(props);
@@ -8,8 +7,6 @@ class Header extends React.Component {
 
     render() {
         return (
-
-
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand" href="#">BitBook</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -35,8 +32,6 @@ class Header extends React.Component {
                     </ul>
                 </div>
             </nav>
-
-
         );
     }
 }
--- src/components/common/mainPage.js
+++ src/components/common/mainPage.js
@@ -11,7 +11,6 @@ class MainPage extends React.Component {
             <Header />
         );
     }
-
 }
 
 export default MainPage;
