b3dde2e22a3d902f86e2841ffe559899f753b63b
BIT Student <classroom@bgit.rs>
2017-11-23 11:18:09 +0100

    add pack to profile DTO in data service

--- src/components/services/serviceData.js
+++ src/components/services/serviceData.js
@@ -1,11 +1,16 @@
 import { BASE_URL } from "../../constants";
 import { APIService } from "./serviceApi";
+import ProfileDTO from "../models/profileDTO";
 
 class ServiceData {
     constructor() { }
 
     getProfile(callback, errorCallback) {
-        APIService.getFromAPI("/profile", callback, errorCallback);
+        APIService.getFromAPI("/profile", response => {
+            const { name, aboutShort, about, avatarUrl, postsCount, commentsCount } = response.data;
+            const profile = new ProfileDTO(name, aboutShort, about, avatarUrl, postsCount, commentsCount);
+            callback(profile);
+        }, errorCallback);
     }
 }
 
