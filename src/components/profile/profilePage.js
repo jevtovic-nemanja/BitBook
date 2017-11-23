178c59c31efe1fa0ca7916859b5aaaa3e876adf5
BIT Student <classroom@bgit.rs>
2017-11-23 11:46:54 +0100

    add load profile from api

--- src/components/profile/profilePage.js
+++ src/components/profile/profilePage.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-
+import { dataService } from "../services/serviceData";
 
 class ProfilePage extends React.Component {
     constructor(props) {
@@ -11,25 +11,46 @@ class ProfilePage extends React.Component {
 
     initState() {
         return {
-            name: "",
-            aboutShort: "",
-            about: "",
-            avatarUrl : "http://3.bp.blogspot.com/_JBHfzEovWs8/S8X3wH9vbTI/AAAAAAAAAPM/O8r2xpeeur0/s1600/batman-for-facebook.jpg",
-            postsCount: 0,
-            commentsCount : 0
+            profile: {
+                name: "",
+                bio: "",
+                about: "",
+                picture: "http://3.bp.blogspot.com/_JBHfzEovWs8/S8X3wH9vbTI/AAAAAAAAAPM/O8r2xpeeur0/s1600/batman-for-facebook.jpg",
+                noOfPosts: 0,
+                noOfComments: 0
+            },
+            error: ""
         };
     }
 
+    componentDidMount() {
+        this.loadProfile();
+    }
+
+    handleNetworkRequestError(error) {
+        if (error.response) {
+            alert(error.response.data.error.message);
+        } else if (error.request) {
+            alert("There is no response from server.");
+        }
+    }
+
+    loadProfile() {
+        dataService.getProfile(profileDTO => this.setState({ profile: profileDTO }),
+            error => this.handleNetworkRequestError(error));
+    }
 
     render() {
+        const { name, bio, about, picture, noOfPosts, noOfComments } = this.state.profile;
+
         return (
             <div className="profilecontent">
-                <img src={this.state.avatarUrl} className="profileimage" />
-                <h1 className="profilename"> Ime i Prezime</h1>
-                <p className="profileabout"> Mali paragraf</p>
-                <p className="profileabout"> Veliki paragraf</p>
-                <div className="profilecounter">post counter </div>
-                <div className="profilecounter"> comment counter</div>
+                <img src={picture} className="profileimage" />
+                <h1 className="profilename">{name}</h1>
+                <p className="profileabout">{bio}</p>
+                <p className="profileabout">{about}</p>
+                <div className="profilecounter">{noOfPosts}</div>
+                <div className="profilecounter">{noOfComments}</div>
             </div>
 
         );
