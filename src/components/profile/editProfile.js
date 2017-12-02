635396785c866e290885b1b1a7438d96c1e023bb
nemanja-jevtovic <jevta993@gmail.com>
2017-12-02 15:50:01 +0100

    move edit profile modal to separate component

---
+++ src/components/profile/editProfile.js
@@ -0,0 +1,155 @@
+import React from "react";
+
+class EditProfile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = this.initState();
+        this.bindEventHandlers();
+    }
+
+    initState() {
+        return {
+            edit: this.props.edit,
+            validationError: ""
+        };
+    }
+
+    bindEventHandlers() {
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.updateProfile = this.updateProfile.bind(this);
+    }
+
+    handleInputChange(event) {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        this.setState(prevState => {
+            prevState.edit[name] = value;
+            prevState.validationError = "";
+            return prevState;
+        });
+    }
+
+    updateProfile(event) {
+        event.preventDefault();
+        const isValid = this.validateInput();
+ 
+        if (isValid) {
+            const dataObject = this.state.edit;
+            this.props.updateProfile(dataObject);
+            this.props.toggleModal();
+        }
+    }
+
+    validateInput() {
+        const { name, email, aboutShort, about, avatarUrl } = this.state.edit;
+
+        if (!name) {
+            this.setState({ validationError: "Please enter a name." });
+            return false;
+        } else if (!email) {
+            this.setState({ validationError: "Please enter an email address." });
+            return false;
+        } else if (!email.includes("@")) {
+            this.setState({ validationError: "A valid email address contains \"@\"." });
+            return false;
+        } else if (!aboutShort) {
+            this.setState({ validationError: "Please enter a short bio." });
+            return false;
+        } else if (!about) {
+            this.setState({ validationError: "Please enter something about yourself." });
+            return false;
+        } else if (!avatarUrl) {
+            this.setState({ validationError: "Please set your profile picture." });
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    render() {
+        const { name, email, aboutShort, about, avatarUrl } = this.state.edit;
+        const { validationError } = this.state;
+
+        return (
+            <div>
+                <div className="editForm">
+                    <form>
+                        <label htmlFor="exampleInputText1">Name</label>
+                        <input
+                            type="text"
+                            className="form-control mb-3"
+                            id="exampleInputText1"
+                            placeholder="Name"
+                            name="name"
+                            value={name}
+                            onChange={this.handleInputChange}
+                        />
+
+                        <label htmlFor="exampleInputEmail1">Contact Email</label>
+                        <input
+                            type="email"
+                            className="form-control mb-3"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                            placeholder="Enter email"
+                            name="email"
+                            value={email}
+                            onChange={this.handleInputChange}
+                        />
+                        <small id="emailHelp" className="form-text text-muted mb-3">We will never share your email with anyone else.</small>
+
+                        <label htmlFor="exampleInputText2">Bio</label>
+                        <textarea
+                            className="form-control mb-3"
+                            id="exampleInputText2"
+                            placeholder="Short Bio"
+                            name="aboutShort"
+                            value={aboutShort}
+                            onChange={this.handleInputChange}
+                        />
+
+                        <label htmlFor="exampleInputText3">About</label>
+                        <textarea
+                            className="form-control mb-3"
+                            id="exampleInputText3"
+                            placeholder="About"
+                            name="about"
+                            rows="5"
+                            value={about}
+                            onChange={this.handleInputChange}
+                        />
+
+                        <label htmlFor="exampleInputText4">Picture</label>
+                        <input
+                            type="text"
+                            className="form-control mb-3"
+                            id="exampleInputText4"
+                            placeholder="Picture URL"
+                            name="avatarUrl"
+                            value={avatarUrl}
+                            onChange={this.handleInputChange}
+                        />
+
+                    </form>
+                </div>
+                <div className="error">
+                    {validationError
+                        ? <p>{validationError}</p>
+                        : <p></p>
+                    }
+                </div>
+                <div>
+                    <button className="btn buttonLight my-2 my-sm-0 saveButtonStyle" onClick={this.updateProfile}>
+                        Save
+                    </button>
+                    <button className="btn btn-outline-danger my-2 my-sm-0 closeButtonStyle" onClick={this.props.toggleModal}>
+                        Close
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default EditProfile;
--- src/components/profile/profilePage.js
+++ src/components/profile/profilePage.js
@@ -5,6 +5,7 @@ import { redirect } from "../services/serviceRedirect";
 
 import { Profile } from "../profile/profile";
 import Modal from "react-modal";
+import EditProfile from "./editProfile";
 
 class ProfilePage extends React.Component {
     constructor(props) {
@@ -30,7 +31,7 @@ class ProfilePage extends React.Component {
                 email: "",
                 aboutShort: "",
                 about: "",
-                avatarUrl: "",
+                avatarUrl: ""
             },
             show: false,
             error: ""
@@ -39,7 +40,6 @@ class ProfilePage extends React.Component {
 
     bindEventHandlers() {
         this.toggleModalShow = this.toggleModalShow.bind(this);
-        this.handleInputChange = this.handleInputChange.bind(this);
         this.updateProfile = this.updateProfile.bind(this);
     }
 
@@ -49,17 +49,6 @@ class ProfilePage extends React.Component {
 
     getProfile() {
         dataService.getProfile(profile => this.loadProfile(profile), error => this.handleNetworkRequestError(error));
-    }
-
-    handleInputChange(event) {
-        const name = event.target.name;
-        const value = event.target.value;
-
-        this.setState(prevState => {
-            prevState.edit[name] = value;
-            prevState.error = "";
-            return prevState;
-        });
     }
 
     handleNetworkRequestError(error) {
@@ -72,11 +61,11 @@ class ProfilePage extends React.Component {
         this.setState({
             profile: profile,
             edit: {
-                editName: profile.name,
-                editEmail: profile.email,
-                editAboutShort: profile.aboutShort,
-                editAbout: profile.about,
-                editAvatarUrl: profile.avatarUrl
+                name: profile.name,
+                email: profile.email,
+                aboutShort: profile.aboutShort,
+                about: profile.about,
+                avatarUrl: profile.avatarUrl
             }
         });
     }
@@ -91,62 +80,13 @@ class ProfilePage extends React.Component {
         }
     }
 
-    updateProfile(event) {
-        event.preventDefault();
-        const isValid = this.validateInput();
-
-        const dataObject = this.state.edit;
-
-        if (isValid) {
-            dataService.updateProfile(dataObject, profile => this.loadProfile(profile),
-                error => this.handleNetworkRequestError(error));
-            this.toggleModalShow();
-        }
-    }
-
-    validateInput() {
-        const { editName, editEmail, editAboutShort, editAbout, editAvatarUrl } = this.state.edit;
-
-        if (!editName) {
-            this.setState({ error: "Please enter a name." });
-            return false;
-        } else if (!editEmail) {
-            this.setState({ error: "Please enter an email address." });
-            return false;
-        } else if (!editEmail.includes("@")) {
-            this.setState({ error: "A valid email address contains \"@\"." });
-            return false;
-        } else if (!editAboutShort) {
-            this.setState({ error: "Please enter a short bio." });
-            return false;
-        } else if (!editAbout) {
-            this.setState({ error: "Please enter something about yourself." });
-            return false;
-        } else if (!editAvatarUrl) {
-            this.setState({ error: "Please set your profile picture." });
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-
-    updateProfile(event) {
-        event.preventDefault();
-        const isValid = this.validateInput();
-
-        const dataObject = this.state.edit;
-
-        if (isValid) {
-            dataService.updateProfile(dataObject, profile => this.loadProfile(profile),
-                error => this.handleNetworkRequestError(error));
-            this.toggleModalShow();
-        }
+    updateProfile(newData) {
+        dataService.updateProfile(newData, profile => this.loadProfile(profile),
+            error => this.handleNetworkRequestError(error));
+        this.toggleModalShow();
     }
 
     getModalStyle() {
-
-
         if (screen.width < 579) {
             return {
                 content: {
@@ -165,8 +105,8 @@ class ProfilePage extends React.Component {
 
                 }
             };
-        } else {
 
+        } else {
             return {
                 content: {
                     position: "absolute",
@@ -186,11 +126,9 @@ class ProfilePage extends React.Component {
         };
     }
 
-
     render() {
 
         const { name, email, aboutShort, about, avatarUrl, postsCount, commentsCount } = this.state.profile;
-        const { editName, editEmail, editAboutShort, editAbout, editAvatarUrl } = this.state.edit;
         const { show, error } = this.state;
 
         return (
@@ -206,80 +144,7 @@ class ProfilePage extends React.Component {
                     </div>
 
                     <Modal isOpen={show} style={this.getModalStyle()} className="editProfileModal" >
-                        <div className="editForm">
-                            <form>
-                                <label htmlFor="exampleInputText1">Name</label>
-                                <input
-                                    type="text"
-                                    className="form-control mb-3"
-                                    id="exampleInputText1"
-                                    placeholder="Name"
-                                    name="name"
-                                    value={this.state.edit.name}
-                                    onChange={this.handleInputChange}
-                                />
-
-                                <label htmlFor="exampleInputEmail1">Contact Email</label>
-                                <input
-                                    type="email"
-                                    className="form-control mb-3"
-                                    id="exampleInputEmail1"
-                                    aria-describedby="emailHelp"
-                                    placeholder="Enter email"
-                                    name="email"
-                                    value={this.state.edit.email}
-                                    onChange={this.handleInputChange}
-                                />
-                                <small id="emailHelp" className="form-text text-muted mb-3">We will never share your email with anyone else.</small>
-
-                                <label htmlFor="exampleInputText2">Bio</label>
-                                <textarea
-                                    className="form-control mb-3"
-                                    id="exampleInputText2"
-                                    placeholder="Short Bio"
-                                    name="aboutShort"
-                                    value={this.state.edit.aboutShort}
-                                    onChange={this.handleInputChange}
-                                />
-
-                                <label htmlFor="exampleInputText3">About</label>
-                                <textarea
-                                    className="form-control mb-3"
-                                    id="exampleInputText3"
-                                    placeholder="About"
-                                    name="about"
-                                    rows="5"
-                                    value={this.state.edit.about}
-                                    onChange={this.handleInputChange}
-                                />
-
-                                <label htmlFor="exampleInputText4">Picture</label>
-                                <input
-                                    type="text"
-                                    className="form-control mb-3"
-                                    id="exampleInputText4"
-                                    placeholder="Picture URL"
-                                    name="avatarUrl"
-                                    value={this.state.edit.avatarUrl}
-                                    onChange={this.handleInputChange}
-                                />
-
-                            </form>
-                        </div>
-                        <div className="error">
-                            {error
-                                ? <p>{error}</p>
-                                : <p></p>
-                            }
-                        </div>
-                        <div>
-                            <button className="btn buttonLight my-2 my-sm-0 saveButtonStyle" onClick={this.updateProfile}>
-                                Save
-                            </button>
-                            <button className="btn btn-outline-danger my-2 my-sm-0 closeButtonStyle" onClick={this.toggleModalShow}>
-                                Close
-                            </button>
-                        </div>
+                        <EditProfile toggleModal={this.toggleModalShow} updateProfile={this.updateProfile} edit={this.state.edit} />
                     </Modal>
                 </div>
             </main>
