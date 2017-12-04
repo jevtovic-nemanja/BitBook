6c77efeac8cc0eb364786ca46a65fcf94bbe6aa5
BIT Student <classroom@bgit.rs>
2017-12-04 14:20:35 +0100

    move upload image

--- src/components/profile/editProfile.js
+++ src/components/profile/editProfile.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import UploadImage from "./uploadImage";
+import UploadImage from "../common/uploadImage";
 
 class EditProfile extends React.Component {
     constructor(props) {
