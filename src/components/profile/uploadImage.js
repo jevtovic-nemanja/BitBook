30f8da1a29d9fe1356e95bce13e6f350814d0b29
BIT Student <classroom@bgit.rs>
2017-12-04 13:33:58 +0100

    add upload picture to profile page

--- src/components/profile/uploadImage.js
+++ src/components/profile/uploadImage.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import FormData from "form-data";
+
+import { dataService } from "../services/serviceData";
+
 class UploadImage extends React.Component {
     constructor(props) {
         super(props);
@@ -11,7 +15,8 @@ class UploadImage extends React.Component {
     initState() {
         return {
             image: "",
-            error: ""
+            previewImage: "",
+            validationError: ""
         };
     }
 
@@ -20,27 +25,43 @@ class UploadImage extends React.Component {
         this.uploadImage = this.uploadImage.bind(this);
     }
 
+    handleError(error) {
+        if (error.response) {
+            this.setState({ validationError: error.response.data.error });
+        } else if (error.request) {
+            this.setState({ validationError: "There is no response from server." });
+        }
+    }
+
     previewImage(event) {
         const image = event.target.files[0];
 
         if (image && image.type.startsWith("image/")) {
             this.setState({
-                image: window.URL.createObjectURL(image),
-                error: ""
+                image: image,
+                previewImage: window.URL.createObjectURL(image),
+                validationError: ""
             });
         } else {
-            this.setState({ error: "Invalid file type! Please select an image file." });
+            this.setState({ validationError: "Invalid file type! Please select an image file." });
         }
     }
 
     uploadImage(event) {
         event.preventDefault();
 
+        const image = this.state.image;
         
+        let postData = new FormData();
+        postData.append("file", image);
+
+        dataService.uploadImage(postData, imageUrl => {
+            this.props.uploadImage(imageUrl);
+        }, error => this.handleError(error));
     }
 
     render() {
-        const { image, error } = this.state;
+        const { image, previewImage, validationError } = this.state;
         const previewStyle = {
             width: 15 + "rem",
             maxHeight: 15 + "rem"
@@ -50,8 +71,13 @@ class UploadImage extends React.Component {
             <div>
                 <label>Select image for upload:</label>
                 <input type="file" onChange={this.previewImage} />
-                <p className="error">{error}</p>
-                <img src={image} style={previewStyle} className="d-block mx-auto" />
+                <div className="error">
+                    {validationError
+                        ? <p>{validationError}</p>
+                        : <p></p>
+                    }
+                </div>
+                <img src={previewImage} style={previewStyle} className="d-block mx-auto" />
                 <button onClick={this.uploadImage} >Upload</button>
             </div>
         );
