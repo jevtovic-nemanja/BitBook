4a1d00ed5a69ee0a1d09c7a5d0b12ec3d55c02ca
nemanja-jevtovic <jevta993@gmail.com>
2017-12-07 20:40:32 +0100

    make header nav links active

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -6,21 +6,30 @@ import { authenticationService } from "../services/serviceAuthentication";
 export const Header = props => {
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: "#3aafa9"}} >
-            <Link to="/" ><p className="navbar-brand font-weight-bold" style={{color: "#17252a", fontSize: "26px"}}>BitBook</p> </Link>
+        <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "#3aafa9" }} >
+            <Link to="/" ><p className="navbar-brand font-weight-bold" style={{ color: "#17252a", fontSize: "26px" }}>BitBook</p> </Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <Link to="/profile" > <li className="nav-item">
-                        <p className="nav-link ml-2 h5" href="#">Profile <span className="sr-only">(current)</span></p>
+                        {location.hash === "#/profile"
+                            ? <p className="nav-link ml-2 h5 active" href="#">Profile </p>
+                            : <p className="nav-link ml-2 h5" href="#">Profile </p>
+                        }
                     </li></Link>
                     <Link to="/feed" ><li className="nav-item">
-                        <p className="nav-link ml-2 h5" href="#">Feed</p>
+                        {location.hash === "#/" || location.hash === "#/feed"
+                            ? <p className="nav-link ml-2 h5 active" href="#">Feed </p>
+                            : <p className="nav-link ml-2 h5" href="#">Feed </p>
+                        }
                     </li></Link>
                     <Link to="/people" ><li className="nav-item">
-                        <p className="nav-link ml-2 h5" href="#">People</p>
+                        {location.hash === "#/people"
+                            ? <p className="nav-link ml-2 h5 active" href="#">People </p>
+                            : <p className="nav-link ml-2 h5" href="#">People </p>
+                        }
                     </li></Link>
                 </ul>
                 <div className="ml-auto">
