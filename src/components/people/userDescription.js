3104c1410d039b1318e0aa93b5d659a1e9ba456c
BIT Student <classroom@bgit.rs>
2017-12-08 17:25:48 +0100

    style people components

--- src/components/people/userDescription.js
+++ src/components/people/userDescription.js
@@ -10,14 +10,15 @@ export const UserDescription = (props) => {
     const lastPostTime = moment.utc(lastPostDate).fromNow();
 
     return (
-        <Link to={`/people/${id}`} >
-            <div className="card mb-4 w-100" style={{ height: 450 + "px"}} >
-                <img src={avatarUrl
-                    ? avatarUrl
-                    : "../../assets/images/batman.jpg"
-                }
-                className="mx-auto mt-3 rounded-circle card-img-top"
-                style={{ width: 250 + "px", height: 230 + "px"}} />
+        <Link to={`/people/${id}`}>
+            <div className="card mb-4 w-100" style={{ height: 450 + "px" }} >
+                <img
+                    src={avatarUrl
+                        ? avatarUrl
+                        : "../../assets/images/batman.jpg"
+                    }
+                    className="mx-auto mt-3 rounded-circle card-img-top"
+                    style={{ width: 250 + "px", height: 230 + "px" }} />
                 <div className="card-body w-100 text-center position-relative">
                     <h3>{name}</h3>
                     <em>{aboutShort}</em>
