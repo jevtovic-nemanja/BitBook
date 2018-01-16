0584425666572c9e7dd1a9c83614abafe14c5ea6
nemanja-jevtovic <jevta993@gmail.com>
2018-01-16 02:58:52 +0100

    fix css code for login page

--- src/components/login/loginPage.js
+++ src/components/login/loginPage.js
@@ -10,16 +10,20 @@ export const LoginPage = props => {
         <div className="background h-100">
             <div className="container">
                 <div className="row text-justify">
-                    <div className="col-12 col-lg-8 offset-lg-2 col-xl-6 welcomeText">
-                        <h1 className="text-center font-weight-bold loginTitle">
+                    <div className="w-100 text-center mt-4">
+                        <h3 className="font-weight-bold login-title">
                             Welcome to BitBook
-                        </h1>
-                        <p className="loginParagraph">
-                            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of (The Extremes of Good and Evil) by Cicero, written in 45 BC This book is a treatise on the theory of ethics, very popular during the Renaissance.
-                        </p>
+                        </h3>
+                    </div>
+                    <div className="col-12 offset-md-1 col-md-10 offset-lg-0 col-lg-6 offset-xl-1 col-xl-5 mb-2">
+                        <div className="login-paragraph">
+                            <p className="d-none d-sm-block">
+                                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of (The Extremes of Good and Evil) by Cicero, written in 45 BC This book is a treatise on the theory of ethics, very popular during the Renaissance.
+                            </p>
+                        </div>
                     </div>
 
-                    <div className="col-12 col-lg-8 offset-lg-2 col-xl-6 welcomeText">
+                    <div className="col-12 offset-md-1 col-md-10 offset-lg-0 col-lg-6 col-xl-5">
                         <Switch>
                             <Route exact path="/login" component={LoginForm} />
                             <Route exact path="/register" component={RegisterForm} />
